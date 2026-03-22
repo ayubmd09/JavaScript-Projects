435c8434ddb3d0c2c738319d2e7c41ff27672173
ayubmd09 <ayubmohammed1439@gmail.com>
2026-03-22 16:23:55 -0400

    Multiple Operators Assignment

--- Baisc JavaScript Projects/Project3_math_operators/JS/main.js
+++ Baisc JavaScript Projects/Project3_math_operators/JS/main.js
@@ -1,11 +1,6 @@
-function addNumbers() {
-    var x = 10;
-    var y = 2;
-
-    var multiply = x * y;
-    var divide = x / y;
+function multiple_operators() {
+    var do_Math = (1 + 4) * 10 / 2 - 5
 
     document.getElementById("Math").innerHTML =
-        x + " × " + y + " = " + multiply + "<br>" +
-        x + " ÷ " + y + " = " + divide;
+        "1 plus 4, multiplied by 10, divided by 2, then subtracted by 5" + do_Math;
 }
