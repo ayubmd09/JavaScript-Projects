bed42f8b0f6f8c8bf4c17a350c7618ae8e9e62a7
ayubmd09 <ayubmohammed1439@gmail.com>
2026-03-21 23:45:56 -0400

    Comment Assignment

--- Baisc JavaScript Projects/Project1_expressions_alert/JS/main.js
+++ Baisc JavaScript Projects/Project1_expressions_alert/JS/main.js
@@ -1,4 +1,4 @@
-var firstVariable = "This is first variable",
-    secondVariable = "This is second variable",
-    thirdVariable = "This is third variable";
+var firstVariable = "This is first variable", //my first variable
+    secondVariable = "This is second variable", //my second variable
+    thirdVariable = "This is third variable"; //my third variable
 document.write(secondVariable);
