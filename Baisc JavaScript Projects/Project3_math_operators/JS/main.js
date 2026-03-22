5ec6c3016536e4db2a441f9015cf1ba37b4bea13
ayubmd09 <ayubmohammed1439@gmail.com>
2026-03-22 16:38:56 -0400

    Project3_Math_Operators Assignment

--- Baisc JavaScript Projects/Project3_math_operators/JS/main.js
+++ Baisc JavaScript Projects/Project3_math_operators/JS/main.js
@@ -1,6 +1,49 @@
-function multiple_operators() {
-    var do_Math = (1 + 4) * 10 / 2 - 5
+// ADDITION FUNCTION
+function addition() {
+    var x = 5 + 3;
+    document.getElementById("Math").innerHTML = "5 + 3 = " + x;
+}
 
-    document.getElementById("Math").innerHTML =
-        "1 plus 4, multiplied by 10, divided by 2, then subtracted by 5" + do_Math;
+// SUBTRACTION FUNCTION
+function subtraction() {
+    var x = 10 - 4;
+    document.getElementById("Math").innerHTML = "10 - 4 = " + x;
+}
+
+// MULTIPLICATION FUNCTION
+function multiplication() {
+    var x = 6 * 2;
+    document.getElementById("Math").innerHTML = "6 × 2 = " + x;
+}
+
+// DIVISION FUNCTION
+function division() {
+    var x = 12 / 3;
+    document.getElementById("Math").innerHTML = "12 ÷ 3 = " + x;
+}
+
+// MODULUS FUNCTION (% gives remainder)
+function modulus() {
+    var x = 10 % 3;
+    document.getElementById("Math").innerHTML = "10 % 3 = " + x;
+}
+
+// INCREMENT FUNCTION (++ adds 1)
+function increment() {
+    var x = 5;
+    x++;
+    document.getElementById("Math").innerHTML = "5 incremented = " + x;
+}
+
+// DECREMENT FUNCTION (-- subtracts 1)
+function decrement() {
+    var x = 5;
+    x--;
+    document.getElementById("Math").innerHTML = "5 decremented = " + x;
+}
+
+// RANDOM NUMBER FUNCTION
+function randomNumber() {
+    var x = Math.random(); // gives number between 0 and 1
+    document.getElementById("Math").innerHTML = "Random number: " + x;
 }
