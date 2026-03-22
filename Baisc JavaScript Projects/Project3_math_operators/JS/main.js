e7d726db548ca110cc7284857ca7621c02f758c4
ayubmd09 <ayubmohammed1439@gmail.com>
2026-03-22 16:11:00 -0400

    Subtraction Assignment

--- Baisc JavaScript Projects/Project3_math_operators/JS/main.js
+++ Baisc JavaScript Projects/Project3_math_operators/JS/main.js
@@ -1,4 +1,4 @@
 function addNumbers() {
-    var result = 5 + 3; // addition operation
-    document.getElementById("Math").innerHTML = "5 + 3 = " + result;
+    var result = 5 - 3; // subtraction operation
+    document.getElementById("Math").innerHTML = "5 - 3 = " + result;
 }
