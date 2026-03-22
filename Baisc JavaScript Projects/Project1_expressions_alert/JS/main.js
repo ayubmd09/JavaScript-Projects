b9b17f6847ceee11657de187b0e73e59969979d7
ayubmd09 <ayubmohammed1439@gmail.com>
2026-03-21 23:52:22 -0400

    Basic JavaScript Projects Assignment

--- Baisc JavaScript Projects/Project1_expressions_alert/JS/main.js
+++ Baisc JavaScript Projects/Project1_expressions_alert/JS/main.js
@@ -1,4 +1,8 @@
 var firstVariable = "This is first variable", //my first variable
     secondVariable = "This is second variable", //my second variable
     thirdVariable = "This is third variable"; //my third variable
-document.write(secondVariable);
+
+var Sent1 = "This is the beginning of the string";
+var Sent2 = " and this is the end of the string";
+document.write(Sent1 + Sent2); //This is an example of concatenate 
+window.alert(5 + 5); // This is an example of expression
